432b51e45f2f126bde3ee2d2ba84ace00b0d778a
Simon Sun <bardrabbit@gmail.com>
2019-02-21 14:25:37 -0500

    Transform string data into numeric

--- homework/homework6/restaurant.js
+++ homework/homework6/restaurant.js
@@ -1,3 +1,17 @@
 d3.csv('restaurant.csv', function(error, data){
-    console.log('csv', data);
+    console.log("1. Explore the first observation:");
+    console.log(data[0]);
+    console.log("---------------------------------------");
+    console.log("2. After converting some numeric data in the string format into the numeric form:");
+    data.forEach(function(d){
+        d.business_id = +d.business_id;
+        d.business_latitude = +d.business_latitude;
+        d.business_longitude = +d.business_longitude;
+        d.business_postal_code = +d.business_postal_code;
+        d.inspection_score = +d.inspection_score;
+    })
+    console.log(data[0]);
+    console.log('-----------------------------------');
+    console.log('3. Look at the whole dataset:');
+    console.log(data);
 })
