803ce91f6705735b2d843b91ee90912b9834e51f
Simon Sun <bardrabbit@gmail.com>
2019-03-19 23:20:14 -0400

    Add restaurants to the map

--- homework/restaurantScoreProject/restaurant.js
+++ homework/restaurantScoreProject/restaurant.js
@@ -13,16 +13,15 @@ d3.queue()
 .defer(d3.json, "sf.json")
 .awaitAll(ready);
 
-/* Create a new projection */
+
 function ready(error, dataArray) {
+
     //topojson transform
     var neighborhoods = topojson.feature(dataArray[1], dataArray[1].objects.SFFind_Neighborhoods);
-    console.log(neighborhoods);
 
     var projection = d3.geoAlbers()
     .fitSize([width, height], neighborhoods);
    
-
     /* Create a path*/
     var path = d3.geoPath()
     .projection(projection);
@@ -43,19 +42,29 @@ function ready(error, dataArray) {
     /* Add restaurants
     get x/y from the lat/long projection
     */
-    // console.log(dataArray[0]);
-    // svg.selectAll(".restaurant-circle")
-    // .data(dataArray[0])
-    // .enter().append("circle")
-    // .attr("r", 2)
-    // .attr("cx", function(d){
-    //     var coords = projection([d.business_latitude, d.business_longitude]);
-    //     return coords[0];
-    // })
-    // .attr("cy", function(d){
-    //     var coords = projection([d.business_latitude, d.business_longitude]);
-    //     return coords[1];
-    // })
+    //transform numeric data to its format
+    dataArray[0].forEach(function (d) {
+        d.business_id = +d.business_id;
+        d.business_latitude = +d.business_latitude;
+        d.business_longitude = +d.business_longitude;
+        d.business_postal_code = +d.business_postal_code;
+        d.inspection_score = +d.inspection_score;
+    });
+   
+    //Put each restaurant as dot on the map
+    svg.selectAll(".restaurant-circle")
+    .data(dataArray[0])
+    .enter().append("circle")
+    .attr("r", 2)
+    .attr("cx", function(d){
+        var coords = projection([d.business_latitude, d.business_longitude]);
+        return coords[0];
+    }) 
+    .attr("cx", function(d){
+        var coords = projection([d.business_latitude, d.business_longitude]);
+        return coords[1];
+    });
+
 
 }
 
